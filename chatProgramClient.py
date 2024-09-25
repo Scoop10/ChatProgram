5f8b64316a2d18caa302fa4ef333982b39e74990
a1812095 <a1812095@adelaide.edu.au>
2024-09-25 22:06:00 +0930

    all comments done

--- chatProgramClient.py
+++ chatProgramClient.py
@@ -5,25 +5,52 @@ from Crypto.PublicKey import RSA
 import hashlib
 import base64
 
-def sendPrivateMessage(clientKey):
+# This function will be called when the user types "Send a private message"
+# The function will take a list of participants from the command line and a message and broadcast it to the server and in turn all clients.
+# clientKey is this clients exported RSA key, destSocket is the socket that the client is connected on and sending the message to
+async def sendPrivateMessage(clientKey, destSocket):
+    
+    ## NEED TO ADD IN HERE A SECTION WHERE I CAN CHOOSE WHICH PARTICIPANTS TO SEND IT TO ##
+
+    # Receive input from the command line
     Message = input()
     
+    # Package the chat section up. Message is still just plain text. Participants will be added from above.
     chat = {
         "participants": [],
         "message": Message
     }
 
-    package = {
+    # Create the symm_keys list with the sending clients key as the first as per the specification
+    symm_keys = [clientKey]
+
+    ## APPEND THE REST OF THE SYMM_KEYS FOR THE PARTICIPANTS HERE ##
+
+    # Package the rest of the message into the desired package as per the specification
+    request = {
         "type": "chat",
         "destination_server": "coming",
         "symm_key": clientKey,
         "chat": chat
     }
 
-async def sendPublicMessage(destSocket):
-    global counter
-    message = input()
+    # Serialise the request int JSON formatted string
+    serialisedRequest = json.dumps(request)
+    # Send the serialised request on the activeSocket
+    await destSocket.send(serialisedRequest)
 
+
+# This function is called when the user inputs "Send a public message"
+# The function receives input from the user and broadcasts it to all clients
+# destSocket is the socket of the connected server
+async def sendPublicMessage(destSocket):
+    ## COUNTER STUFF TO BE DEALT WITH ## 
+    global counter
+
+    print("Enter message to send publicly: ")
+    # Receive input from the user
+    message = input()
+    # Request packaging
     request = {
         "type":"signed_data",
         "data": {
@@ -35,25 +62,31 @@ async def sendPublicMessage(destSocket):
         "signature":"coming"
     }
 
+    # Serialise request into JSON formatted string
     serialisedRequest = json.dumps(request)
-    
+    # Schedule serialised request to be sent to everyone via the server
     await destSocket.send(serialisedRequest)
+    ## COUNTER STUFF TO BE DEALT WITH ## 
     counter = counter + 1
 
+# This function is called when the user inputs "Who's online?"
+# The function sends a client_list_request message to the connected server
+# activeSocket is the server socket
 async def getClientList(activeSocket):
-    global counter
+    # Request packaging
     request = {
         "type": "client_list_request"
     }
-
+    # Serialise request into JSON formatted string
     serialisedRequest = json.dumps(request)
-
+    # Schedule serialised request to be sent to everyone via the server
     await activeSocket.send(serialisedRequest)
 
-
+# This function is called when the client to the specified server
 async def sendHelloMessage(activeSocket):
+    ## COUNTER TO BE FIXED ##
     global counter
-
+    # Request packaging as per specification
     request = {
         "type":"signed_data",
         "data": {
@@ -61,122 +94,198 @@ async def sendHelloMessage(activeSocket):
             "public_key" : exportedPublicKey
         },
         "counter": counter,
-        "signature":"coming"
+        "signature":"coming" # NEEDS IMPLEMENTATION
     }
 
+    # Serialise request into JSON formatted string
     serialisedRequest = json.dumps(request)
-
+    # Schedule serialised request to be sent to everyone via the server
     await activeSocket.send(serialisedRequest)
+    ## TO BE FIXED
     counter = counter + 1
 
+# This function is called when the client receives a client_list message from the connected server
+# The function updates the fingerprints of all connected clients
 async def updateFingerprints():
+    # Clear the fingerprints list
     fingerprints.clear()
+    # Iterate through all connected clients
     for client in connectedClients:
+        # Take the SHA256 hash of the current public RSA key
         sha256_hash = hashlib.sha256(client.export_key()).digest()
+        # Base 64 encode the hash taken above to get the clients fingerprint
         clientFingerprint = base64.b64encode(sha256_hash).decode('utf-8')
+        # Append the updated fingerprint to the list
         fingerprints.append(clientFingerprint)
-    
+    # TEMPORARY CONFIRMATION
     print(fingerprints)
 
+# This function runs as a continual process to receive messages over the server to client connection
+# The function is the main handler of all incoming messages and calls the relevant functions whenever a message is received
+# websocket is the open socket which is connected to the server. stop_event is an Asyncio event which is set to true when the server disconnects
 async def receiveMessages(websocket, stop_event):
+    ## COUNTER WILL BE FIXED
     global counter
+    ## TO BE IMPLEMENTED
     global connectedServers
+    ## TO BE IMPLEMENTED
+    global counters
+    # While the stop_event is not set
     while not stop_event.is_set():
         try:
+            # Receive any messages sent over the socket
             response = await websocket.recv()
+            # Deserialise the message
             responseMessage = json.loads(response)
+            # TEMPORARY CONFIRMATION
             print(f"Received response from server: ", responseMessage)
-
+            # If a client_list message
             if responseMessage["type"] == "client_list":
+                # Clear the connectedClients list ready to refresh it
                 connectedClients.clear()
-
-                receivedServerList = responseMessage["servers"]
-                connectedServers = receivedServerList
-
-                for server in receivedServerList:
-                    currentServerClients = server["clients"]
-                    for clients in currentServerClients:
+                # Extract the servers list from the message
+                # Set the global connectedServerList to be the servers sent in the client_list message 
+                connectedServers = responseMessage["servers"]
+                # Iterate through each server in the client_list message
+                for server in responseMessage["servers"]:
+                    # Iterate through each client in the current server
+                    for clients in server["clients"]:
+                        # Append the current client onto the connectedClients list
                         connectedClients.append(RSA.import_key(clients))
-
+                # TEMPORARY CONFIRMATION
                 print(connectedClients)
-
+                # Update the fingerprints to match the current connected clients list
                 await asyncio.gather(updateFingerprints())
-
-            elif responseMessage["type"] == "client_update":
-                connectedClients.clear()
-
-                for client in responseMessage["clients"]:
-                    connectedClients.append(RSA.import_key(client))
-                
-                print(connectedClients)
-
-                await asyncio.gather(updateFingerprints())
-
-
+            # Else if the received message is signed_data
             elif responseMessage["type"] == "signed_data":
+                # Extract the data portion of the message
                 data = responseMessage["data"]
+                # Extract the type from the data to see what type of signed_data it is
                 type = data["type"]
-                counter = responseMessage["counter"]
+                # If the data is a private chat
+                if type == "chat":
+                    # Extract the chat from the message
+                    chat = responseMessage["chat"]
+                    # Extract the participants list
+                    participantsList = chat["participants"]
+                    ## CHECK IF CLIENT HAS A FINGERPRINT STORED THAT MATCHES THE SENDER ## 
 
-                if type == "public_chat":
+                    ## ADD IN THE COUNTER CHECK AND UPDATE HERE ##
+
+                    ## ADD IN THE DECRYPTION CODE HERE ##
+                    
+                # Else if the received message is a public chat message
+                elif type == "public_chat":
+                    ## CHECK IF CLIENT HAS A FINGERPRINT STORED THAT MATCHES THE SENDER ## 
+                    
+                    # TEMPORARY CONFIRMATION - THIS NEEDS TO BE FORMATTED BETTER
                     print("Public message received from: ", data["sender"])
 
+                    ## ADD IN THE COUNTER UPDATE HERE ##
+
+        # If an exception occurs because the server has closed it's connection
         except websockets.ConnectionClosed:
             print("Connection closed by server")
+            # Set stop_event to true (will shut down the client)
             stop_event.set()
+            # Break the loop
             break
+        # If any other exception occurs
         except Exception as e:
             print("Exception: ", {e})
+            # Set stop_event to true (will shut down the client)
             stop_event.set()
+            # Break the loop
             break
 
+# This function run the blocking input function in a non-blocking way by separating it into it's own loop
+# The function asks for user input which is used within the userInterface function
 async def getUserInput(stop_event):
+    # Get a new asyncio event loop
     loop = asyncio.get_event_loop()
+    # While the stop_event is not true
     while not stop_event.is_set():
+        # This is just to make sure that the "Waiting for user input: " line is printed after a response is received. ## THIS NEEDS SOME MORE THOUGHT ##
+        await asyncio.sleep(0.5)
+        # Set userInput to be user input. This run_in_executor is what runs the input function in a non-blocking way
         userInput = await loop.run_in_executor(None, input, "Waiting for user input: ")
-        if stop_event.is_set():
-            break
+        # Yield is just a fancy return
         yield userInput
 
+# This function deals with all the user input and calls the relevant functions. 
+# It is run concurrently with the receive function and takes in the users input from the executor loop
+# clientSocket is the open server connection
 async def userInterface(clientSocket, stop_event):
+    # For any input which is entered by the user
     async for command in getUserInput(stop_event):
+        # If the user enters "Who's online"
         if command == "Who's online?":
+            # Call the getClientList function to send a client_list_request
             await asyncio.gather(getClientList(clientSocket))
+        # Else If the user enters "Send a public message"
         elif command == "Send a public message":
+            # Call the sendPublicMessage function to send a public message
             await asyncio.gather(sendPublicMessage(clientSocket))
+        # Else If the user enters "Sign off"
         elif command == "Sign off":
+            # CLose the socket connection and break the loop. Will shut down the client. 
             await clientSocket.close()
             break
 
-
-async def main():
+# Main client startup function
+async def main(server):
+    # Create a new asyncio event
     stop_event = asyncio.Event()
-    server = "ws://localhost:8765"
+    # Connect the websocket on the above server and assign it to be clientSocket
     async with websockets.connect(server) as clientSocket:
+        # Send a hello message as the first message to the server
         await asyncio.gather(sendHelloMessage(clientSocket))
+        # Create the receiving messages task to handle the incoming messages from the connected server
         receive_task = asyncio.create_task(receiveMessages(clientSocket, stop_event))
+        # Create the user input and handling of user input task which will call all the client side functions
         send_task = asyncio.create_task(userInterface(clientSocket, stop_event))
+        # Begin both tasks concurrently
         await asyncio.gather(receive_task, send_task)
-        
 
+# Basic initialisation code
 if __name__ == "__main__":
-
+    ## NEEDS TO BE FIXED ## 
     counter = 1
 
+    # Initialise the empty lists:
+    # connectedServers stores all the currently connected servers
     connectedServers = []
+    # connectedClients stores all the currently connnected clients
     connectedClients = []
+    # fingerprints will store all the currently connected clients fingerprints in the same order as the connectedClients list
     fingerprints = []
-    names = []
-
+    # counters will store [fingerprint, counter] of all clients that have sent messages to this client. This will keep track of the latest counters for each client
+    # and will be the fundamental for the counter functionality. The first entry is this clients fingerprint and counter
+    counters = []
+    # Generate this clients private RSA key
     myPrivateKey = RSA.generate(2048)
+    # Generate the matching public RSA key
     myPublicKey = myPrivateKey.public_key()
-
+    # Export that public key to a string
     exportedPublicKey = myPublicKey.export_key().decode('utf-8')
-
+    # Take the hash of the string public key
     sha256_hash = hashlib.sha256(myPublicKey.export_key()).digest()
-
+    # Base 64 encode that hash to get the fingerprint of this client
     myFingerprint = base64.b64encode(sha256_hash).decode('utf-8')
+    # Store this fingerprint, counter pair
+    counters.append([myFingerprint, 1])
+    # Until user inputs "Shut down"
+    while True:
+        # Ask the user what server they would like to connect to 
+        server = input("Enter the server name you would like to connect to: ")
+        # If user inputs "Shut down"
+        if server == "Shut down":
+            # Break the loop - This will shut the program down
+            break
+        # Start the client connection to the server
+        asyncio.run(main(server))
+        # Print the sign out message
+        print("Signed out of ", server)
 
-    asyncio.run(main())
-
-    print("Ending Client Program")
+    # Ending
+    print("Shutting Down Client Program")
--- chatProgramServer.py
+++ chatProgramServer.py
@@ -3,126 +3,184 @@ import websockets
 from Crypto.PublicKey import RSA
 import asyncio
 
+# This function is called when a hello message is received from a client
+# It checks if the client who sent the hello is in the currently connected clients list.
+# If not it adds the clients socket to the socket list and the clients public key to the connected clients list
+# data is the data which has been extracted from the hello message and activeSocket is the socket which the client is currently connected on
 async def helloMessage(data, activeSocket):
+    # Create a new RSA key object from the public_key string which is sent in the data package
     newKey = RSA.import_key(data["public_key"])
+    # If the key is not in the current connected client_list
     if newKey not in client_list:
+        # Append the current client socket (activeSocket) to the servers socketList (which keeps track of all open sockets)
         socketList.append(activeSocket)
+        # Append the new RSA key object to the currently connected client_list (This is at the same index as the socket which the client is connected on so that they are effectively linked)
         client_list.append(newKey)
 
+# This function is called when the server receives a client_list_request message.
+# The function sends a client_list message back to the socket which requested it
+# activeSocket is the socket which the client who requested the list is connected on
 async def getClientList(activeSocket):
+
+# THIS CODE SHOULD POSSIBLY BE MOVED TO THE HELLO MESSAGE FUNCTION
+
+    # Initialise an empty list to store the RSA public key strings in
     stringClientList = []
+    # Iterate through all the currently connected clients
     for keys in client_list:
+        # For each currently connected client export the equivalent RSA key string and append that string to the stringClientList
         stringClientList.append(keys.export_key().decode())
 
+    # Update the server_list with the currently connected clients
     server_list[0]["clients"] = stringClientList
 
+# END OF CODE TO POSSIBLE BE MOVED
+
+    # Request needs to have the below format as per the specification
+    # server_list is the above list of exported RSA public keys
     request = {
         "type" : "client_list",
         "servers" : server_list,
     }
     
+    # Serialise the request into a JSON formatted string
     serialisedClientList = json.dumps(request)
-
+    # Send the serialisedClientList over the socket corresponding to the client who requested the client_list
     await activeSocket.send(serialisedClientList)
 
+    # SERVER SIDE CONFIRMATION - NOT NECESSARY
     print("Sent client list")
 
     return
 
+
+# This function is called when a server requests a client_update because a client has either said hello or disconnected
+# The function compiled a client_update message and sends it to all servers (THIS HAS NOT YET BEEN IMPLEMENTED!!!!!)
 async def sendClientUpdate():
+    # Initialise an empty list to store the RSA public key strings in
     stringClientList = []
+    # Iterate through all the currently connected clients
     for keys in client_list:
         stringClientList.append(keys.export_key().decode())
-
+    # Request needs to have the below format as per the specification
+    # server_list is the above list of exported RSA public keys
     request = {
         "type" : "client_update",
         "clients" : stringClientList,
     }
-    
+    # Serialise the request into a JSON formatted string
     serialisedClientList = json.dumps(request)
 
-    # THIS NEEDS TO BE SENT TO EACH SERVER!!!!!!!!
+    ## THIS NEEDS TO BE SENT TO EACH SERVER!!!!!!!! ##
 
+# This function is called when either a public or a private chat is received from a client on this server
+# The function sends the message to every server
+# Data is a serialised JSON String package which is to be sent to all servers
+## THIS NEEDS TO BE COMPLETED ##
+async def sendToAllServers(data):
+    ## UNCOMMENT AND FINISH THIS CODE WHEN THE SERVER CONNECTIONS HAVE BEEN DEALT WITH ##
 
-    for client in socketList:
-        await asyncio.gather(getClientList(client))
-
+    # Iterate through all servers
+    # for server in serverSocketList:
+        # Send the public_chat to all servers
+    #     await server.send(serialisedData)
     return
 
+# This function is the main handler of all clients. Each client has an individual socket
+# The general purpose of this function is to receive messages from clients and to call or perform the correct functions based on the type of message received
+# activeSocket is the current socket which the client in this async process is running on
 async def clientHandler(activeSocket):
+    # COUNTER NEEDS TO BE DEALT WITH THIS WILL BE REMOVED
     global counter
+    # Try is used so that if an exception occurs it is handled properly
     try:
+        # This loop should run until an exception is encountered (such as the socket closing)
         while True:
             try:
+                # Wait for a message to be received on the server socket. Timeout = 5 is to detect when the client drops off. 
+                # (Every 5 seconds a ping will be sent to test if the client is still online)
                 message = await asyncio.wait_for(activeSocket.recv(), timeout=5)
+                # When a message is received deserialised the received JSON object
                 data = json.loads(message)
+                # SERVER SIDE CONFIRMATION
                 print(f"Received message: {data}")
-                if data["type"] == "signed_data":
-                    if counter >= data["counter"]:
-                        print("Counter exception!")
-                        
-
-                    # WIll need to insert decryption code here!!
-
-
-
+                # If the data received is a signed_data package
+                if data["type"] == "signed_data":                 
+                    # Get the actual data segment of the received package
                     receivedData = data["data"]
-
+                    # If a hello messahe has been received
                     if receivedData["type"] == "hello":
+                        # Call the helloMessage function as a coroutine to execute concurrently
                         await asyncio.gather(helloMessage(receivedData, activeSocket))
+                        # SERVER SIDE CONFIRMATION
                         print("Hello message Received")
-                        await asyncio.gather(sendClientUpdate())
+                        ## THIS WILL BE REMOVED AS I NEED TO WORK ON THIS ##
                         counter = data["counter"]
+                        # Go back to the start of the loop and wait for the next message
                         continue
+                    # If a public_chat type of message has been received
                     elif receivedData["type"] == "public_chat":
+                        # SERVER SIDE CONFIRMATION
                         print("Received public chat message")
-                        await asyncio.gather(receivePublicMessage(receivedData))
+                        # Convert the public_chat into a serialised JSON String
+                        serialisedData = json.dumps(data)
+                        # Schedule the data to be broadcast to all servers
+                        await asyncio.gather(sendToAllServers(serialisedData))
+                        ## THIS WILL BE REMOVED AS I NEED TO WORK ON THIS ##
                         counter = data["counter"]
+                        # Go back to the start of the loop and wait for the next message
                         continue
+                # Else if the received message is a client_list_request
                 elif data["type"] == "client_list_request":
+                    # Call the getClientList function in a coroutine with the activeSocket as the arg
                     await asyncio.gather(getClientList(activeSocket))
+                    # Go back to the start of the loop and wait for the next message
                     continue
+            # If the socket doesn't receive a message for 5 seconds
             except asyncio.TimeoutError:
+                # Send a ping to check if the socket is still online
                 await activeSocket.ping()
-
+    # If an exception occurs as the client disconnected
     except websockets.ConnectionClosed:
+        # Find the socket which disconnected
         index = socketList.index(activeSocket)
+        # SERVER SIDE CONFIRMATION
         print(client_list[index], " Disconnected. Connection Closed!")
     finally:
-        index = socketList.index(activeSocket)
+        # Remove the disconnected socket from the socketList
         socketList.remove(activeSocket)
+        # Remove the disconnected client  from the connected clients
         client_list.remove(client_list[index])
+        # Send a client update to all servers as a client disconnected
         await asyncio.gather(sendClientUpdate())
 
-async def receivePublicMessage(data):
-    serialisedData = json.dumps(data)
-    await asyncio.gather(sendPublicMessage(serialisedData))
-
-async def sendPublicMessage(data):
-    for client in socketList:
-        await client.send(data)
-
+# This function is used to start the server
 async def startServer():
+    # Start the server on the host and port specified. "URL" will be ws://host:port
     server = await websockets.serve(clientHandler, host, port)
+    # Wait until the server is closed then return
     await server.wait_closed()
 
 
-# connection code
-
+# Main
 if __name__ == '__main__':
+    # Specify the port
     port = 8765
-    # host = "192.168.20.49" # Laptop
+    # Specify the host
     host = "localhost" # PC
-
+    ## THIS WILL BE REMOVED ONCE COUNTER STUFF IS FIGURED OUT ## 
     counter = 0
-
+    # SERVER SIDE CONFIRMATION
     print("Starting Server on IP: ", host, " and port: ", port)
+    # Initialise empty lists for the required info to be stored:
+    # socketList stores active sockets, client_list stores active clients RSA keys
     socketList = []
     client_list = []
+    
+    # server_list will have all of the neighborhood servers manually entered
     server_list = []
 
     # Append this server to the server_list
-
     server_list.append({"address" : host, "clients" : client_list})
-
+    # Start the server
     asyncio.run(startServer())
